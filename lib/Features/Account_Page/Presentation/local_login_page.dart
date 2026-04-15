b359367f2176206eb95a3e92d69ae9f3b67aaf20
a.shakouri <alishakoori89@gmail.com>
2026-04-15 13:23:52 +0330

    add top padding for local login text field

--- lib/Features/Account_Page/Presentation/local_login_page.dart
+++ lib/Features/Account_Page/Presentation/local_login_page.dart
@@ -59,113 +59,118 @@ class _LocalLoginPageState extends State<LocalLoginPage> {
                         ),
                         child: IntrinsicHeight(
                           child: SingleChildScrollView(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                AppSpace.heightSpace_128,
-                                NeoBankLogo(
-                                  logoColor: AppColors.splashGradiantColor1,
-                                  logoWidth: 98,
-                                  logoHeight: 24,
-                                  space: 5,
-                                ),
-                                AppSpace.heightSpace_32,
-                                PassField(
-                                  passFieldController: localPassController,
-                                  onpassFieldsIsFill: onLocalPassFieldsIsFill,
-                                ),
-                                AppSpace.heightSpace_16,
-                            
-                                // ورود
-                                localPassFieldsIsFill != true
-                                    ? Align(
-                                        alignment: Alignment.topRight,
-                                        child: Padding(
-                                          padding: const EdgeInsets.only(
-                                            right: 20,
-                                            bottom: 5,
-                                          ),
-                                          child: Text(
-                                            'رمز عبور خود را وارد نمایید.',
-                                            style: TextStyle(
-                                              color: Colors.red,
-                                              fontWeight: FontWeight.bold,
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                top: 50
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  AppSpace.heightSpace_128,
+                                  NeoBankLogo(
+                                    logoColor: AppColors.splashGradiantColor1,
+                                    logoWidth: 98,
+                                    logoHeight: 24,
+                                    space: 5,
+                                  ),
+                                  AppSpace.heightSpace_32,
+                                  PassField(
+                                    passFieldController: localPassController,
+                                    onpassFieldsIsFill: onLocalPassFieldsIsFill,
+                                  ),
+                                  AppSpace.heightSpace_16,
+
+                                  // ورود
+                                  localPassFieldsIsFill != true
+                                      ? Align(
+                                          alignment: Alignment.topRight,
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                              right: 20,
+                                              bottom: 5,
+                                            ),
+                                            child: Text(
+                                              'رمز عبور خود را وارد نمایید.',
+                                              style: TextStyle(
+                                                color: Colors.red,
+                                                fontWeight: FontWeight.bold,
+                                              ),
                                             ),
                                           ),
-                                        ),
-                                      )
-                                    : Text(''),
-                                Container(
-                                  margin: EdgeInsets.only(left: 20, right: 20),
-                                  width: double.infinity,
-                                  child: ElevatedButton(
-                                    style: ButtonStyle(
-                                      backgroundColor:
-                                          WidgetStateProperty.all<Color>(
-                                            AppColors.splashGradiantColor1,
-                                          ),
-                                      shape:
-                                          WidgetStateProperty.all<
-                                            RoundedRectangleBorder
-                                          >(
-                                            RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.circular(
-                                                7.0,
-                                              ), // Adjust for desired corner radius
+                                        )
+                                      : Text(''),
+                                  Container(
+                                    margin: EdgeInsets.only(left: 20, right: 20),
+                                    width: double.infinity,
+                                    child: ElevatedButton(
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                            WidgetStateProperty.all<Color>(
+                                              AppColors.splashGradiantColor1,
                                             ),
-                                          ),
-                                    ),
-                                    onPressed: () async {
-                                      final localPass = await LocalStorage.read(
-                                        'local_password',
-                                      );
-
-
-
-                                      if(localPassController.text.length < 4){
-
-                                        AppSnackBar.errorTop(
-                                          context,
-                                          'پسورد را کامل وارد نمایید.',
+                                        shape:
+                                            WidgetStateProperty.all<
+                                              RoundedRectangleBorder
+                                            >(
+                                              RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(
+                                                  7.0,
+                                                ), // Adjust for desired corner radius
+                                              ),
+                                            ),
+                                      ),
+                                      onPressed: () async {
+                                        final localPass = await LocalStorage.read(
+                                          'local_password',
                                         );
-                                      } else{
-                                        if (localPassController.text ==
-                                            localPass.toString()) {
 
-                                          ButtonInternetChecker.checkInternet(
-                                            context: context,
-                                            onSuccess: () {
-                                              context.go('/main_page', extra: 0);
-                                            },
-                                          );
 
-                                        } else {
+
+                                        if(localPassController.text.length < 4){
+
                                           AppSnackBar.errorTop(
                                             context,
-                                            'پسورد اشتباه است.',
+                                            'پسورد را کامل وارد نمایید.',
                                           );
-                                        }
-                                      }
-                            
+                                        } else{
+                                          if (localPassController.text ==
+                                              localPass.toString()) {
 
-                                    },
-                                    child: SizedBox(
-                                      width: double.infinity,
-                                      child: Center(
-                                        child: Text(
-                                          'تایید',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 16,
+                                            ButtonInternetChecker.checkInternet(
+                                              context: context,
+                                              onSuccess: () {
+                                                context.go('/main_page', extra: 0);
+                                              },
+                                            );
+
+                                          } else {
+                                            AppSnackBar.errorTop(
+                                              context,
+                                              'پسورد اشتباه است.',
+                                            );
+                                          }
+                                        }
+
+
+                                      },
+                                      child: SizedBox(
+                                        width: double.infinity,
+                                        child: Center(
+                                          child: Text(
+                                            'تایید',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                            ),
                                           ),
                                         ),
                                       ),
                                     ),
                                   ),
-                                ),
-                            
-                                AppSpace.heightSpace_128,
-                              ],
+
+                                  AppSpace.heightSpace_128,
+                                ],
+                              ),
                             ),
                           ),
                         ),
