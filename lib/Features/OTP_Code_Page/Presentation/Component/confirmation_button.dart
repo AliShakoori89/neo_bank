c9f3adc15541cf1e9be60daead76a1506c2d78c0
a.shakouri <alishakoori89@gmail.com>
2026-04-18 14:11:32 +0330

    handel set pass key page

---
+++ lib/Features/OTP_Code_Page/Presentation/Component/confirmation_button.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:neo_bank_mehr_iran/Core/Const/app_colors.dart';
+import 'package:neo_bank_mehr_iran/Core/Utils/app_snackbar.dart';
+import 'package:neo_bank_mehr_iran/Features/OTP_Code_Page/Presentation/Bloc/OTP_Code_Check/otp_code_check_bloc.dart';
+import 'package:neo_bank_mehr_iran/Features/OTP_Code_Page/Presentation/Bloc/OTP_Code_Check/otp_code_check_event.dart';
+import 'package:neo_bank_mehr_iran/Features/OTP_Code_Page/Presentation/Bloc/OTP_Code_Check/otp_code_check_state.dart';
+import 'package:neo_bank_mehr_iran/Features/Set_Pass_Page/Presentation/Bloc/Local_Pass_Bloc/local_pass_bloc.dart';
+import 'package:neo_bank_mehr_iran/Features/Set_Pass_Page/Presentation/Bloc/Local_Pass_Bloc/local_pass_state.dart';
+
+import '../../../../Core/Utils/App_Lock/Internet/button_internet_checker.dart';
+import '../../../Account_Page/Data/Data_Sources/Local/token_storage.dart';
+import '../../../Set_Pass_Page/Presentation/Bloc/Local_Pass_Bloc/local_pass_event.dart';
+
+class ConfirmationButton extends StatefulWidget {
+  const ConfirmationButton({
+    super.key,
+    required this.otpController,
+    required this.isOtpComplete,
+    required this.secretKey,
+    required this.deviceId,
+  });
+
+  final TextEditingController otpController;
+  final bool isOtpComplete;
+  final String secretKey;
+  final String deviceId;
+
+  @override
+  State<ConfirmationButton> createState() => _ConfirmationButtonState();
+}
+
+class _ConfirmationButtonState extends State<ConfirmationButton> {
+
+  @override
+  void initState() {
+    BlocProvider.of<LocalPassBloc>(context).add(IsFirstLoginEvent());
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LocalPassBloc, LocalPassState>(
+      builder: (context, state) {
+
+        bool isFirstLocalLogin = state.isFirstLoginStatus;
+
+        return BlocConsumer<OtpCodeCheckBloc, OtpCodeCheckState>(
+          listener: (context, state) {
+
+            if (state.status.isSuccess) {
+              if (state.otpLoginStatus) {
+                if(isFirstLocalLogin){
+                  context.go('/set_pass_page');
+                }else{
+                  context.go('/main_page');
+                }
+              } else {
+                AppSnackBar.errorTop(context, state.otpLoginMessage);
+              }
+            }
+          },
+          builder: (context, state) {
+            return widget.isOtpComplete
+                ? SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            AppColors.splashGradiantColor1, // حالت غیرفعال
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+
+                      onPressed: () {
+                        final otp = widget.otpController.text;
+
+                        ButtonInternetChecker.checkInternet(
+                          context: context,
+                          onSuccess: () {
+                            context.read<OtpCodeCheckBloc>().add(
+                              OtpCodeCheckValueEvent(
+                                deviceId: widget.deviceId,
+                                otpCode: otp,
+                                secretKey: widget.secretKey,
+                              ),
+                            );
+                          });
+
+                        context.read<OtpCodeCheckBloc>().add(
+                          OtpCodeCheckValueEvent(
+                            deviceId: widget.deviceId,
+                            otpCode: otp,
+                            secretKey: widget.secretKey,
+                          ),
+                        );
+
+
+                      }, // 👈 وقتی null باشه دکمه قفله
+
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: const [
+                            Text(
+                              'تایید و ادامه',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Positioned(
+                              left: 5,
+                              child: Icon(Icons.arrow_forward, size: 24),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                : Container(
+                    width: double.infinity,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade400,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: const [
+                          Text(
+                            'تایید و ادامه',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Positioned(
+                            left: 5,
+                            child: Icon(Icons.arrow_forward, size: 24),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+          },
+        );
+      }
+    );
+  }
+}
