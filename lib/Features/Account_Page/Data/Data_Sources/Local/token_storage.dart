63a62212011a660a7a90688d62f9fd8f059c552e
a.shakouri <alishakoori89@gmail.com>
2026-05-12 10:27:42 +0330

    add clear method and delete clearPrefsExcept

--- lib/Features/Account_Page/Data/Data_Sources/Local/token_storage.dart
+++ lib/Features/Account_Page/Data/Data_Sources/Local/token_storage.dart
@@ -45,50 +45,30 @@ class LocalStorage {
   //   return expire == null || DateTime.now().isAfter(DateTime.parse(expire));
   // }
 
-  static clearPrefsExcept(List<String> keepKeys) async {
-    final prefs = await SharedPreferences.getInstance();
-
-    // ذخیره مقدار کلیدهای مهم
-    final Map<String, Object> backup = {};
-
-    for (final key in keepKeys) {
-      if (prefs.containsKey(key)) {
-        final value = prefs.get(key);
-        if (value != null) {
-          backup[key] = value;
-        }
-      }
-    }
-
-    // پاک کردن همه چیز
-    await prefs.clear();
-
-    // برگرداندن کلیدهای مهم
-    for (final entry in backup.entries) {
-      final key = entry.key;
-      final value = entry.value;
-
-      if (value is String)
-        await prefs.setString(key, value);
-      else if (value is int)
-        await prefs.setInt(key, value);
-      else if (value is bool)
-        await prefs.setBool(key, value);
-      else if (value is double)
-        await prefs.setDouble(key, value);
-      else if (value is List<String>) {
-        await prefs.setStringList(key, value);
-      }
-    }
-  }
+  // static Future<void> clearPrefsExcept() async {
+  //   try {
+  //     final List<String> keysToDelete = [];
+  //     final List<String> allKey = ['local_password' , 'isDarkTheme', 'secret_key', 'access_token'];
+  //
+  //     for (String key in allKey) {
+  //       if (key != 'isDarkTheme') {
+  //         keysToDelete.add(key);
+  //       }
+  //     }
+  //
+  //     for (String key in keysToDelete) {
+  //       await _prefs.remove(key);
+  //       print('Deleted key: $key');
+  //     }
+  //
+  //     print('Successfully cleared all encrypted preferences except local_password.');
+  //
+  //   } catch (e) {
+  //     print("Error clearing encrypted preferences: $e");
+  //   }
+  // }
 
   static Future<void> clear() async {
-    try {
-      await _prefs.remove('token');
-      await _prefs.remove('token_expire_at');
-    } catch (e) {
-      print("Error removing encrypted value: $e");
-    }
-
+    await _prefs.clear();
   }
 }
