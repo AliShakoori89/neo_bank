9e4f104c3e099a59164a159577218b6ad9678eea
a.shakouri <alishakoori89@gmail.com>
2026-05-16 16:14:58 +0330

    add custom header

---
+++ lib/Features/Home_Page/Presentation/Component/Charge_Internet_Page/Presentation/Component/custom_header.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class CustomHeader extends StatelessWidget {
+  const CustomHeader({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 92,
+      width: double.infinity,
+      padding: const EdgeInsets.only(
+        top: 40, // spacing-5xl (مثلاً)
+        right: 24, // spacing-3xl
+        bottom: 16, // spacing-lg
+        left: 24, // spacing-3xl
+      ),
+      decoration: BoxDecoration(
+        color: Theme.of(context).appBarTheme.backgroundColor,
+        border: Border(
+          bottom: BorderSide(
+            color: Theme.of(context).colorScheme.surfaceDim,
+            width: 1,
+          ),
+        ),
+      ),
+      child: Align(
+          alignment: Alignment.centerRight,
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.centerRight,
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: (){
+                    context.pop();
+                  },
+                ),
+              ),
+              Center(
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primaryFixed,
+                  ),
+                ),
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
