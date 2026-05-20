8c876625f551f40b613ddc6511646a599f399dd5
a.shakouri <alishakoori89@gmail.com>
2026-05-20 15:12:38 +0330

    add wallet detail package event

--- lib/Features/Home_Page/Presentation/Bloc/Wallet_Bloc/wallet_bloc.dart
+++ lib/Features/Home_Page/Presentation/Bloc/Wallet_Bloc/wallet_bloc.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:neo_bank_mehr_iran/Features/Home_Page/Domain/Repository/get_internet_packages_repository.dart';
 import 'package:neo_bank_mehr_iran/Features/Home_Page/Presentation/Bloc/Wallet_Bloc/wallet_event.dart';
 import 'package:neo_bank_mehr_iran/Features/Home_Page/Presentation/Bloc/Wallet_Bloc/wallet_state.dart';
 
@@ -10,17 +9,40 @@ class WalletBloc extends Bloc<WalletEvent, WalletState> {
   final WalletRepository walletRepository;
 
   WalletBloc( this.walletRepository) : super(WalletState.initial()) {
-    on<WalletDetailsPackages>(_onWalletDetailsPackagesEvent);
+    on<WalletDetailsPackagesEvent>(_onWalletDetailsPackagesEvent);
+    on<BuyPackagesEvent>(_onBuyPackagesEvent);
   }
 
   void _onWalletDetailsPackagesEvent(
-      WalletDetailsPackages event,
+      WalletDetailsPackagesEvent event,
       Emitter<WalletState> emit,
       ) async {
     try {
       emit(state.copyWith(status: WalletStateStatus.loading));
 
       final walletDetails = await walletRepository.getWalletDetails();
+      print(walletDetails);
+
+      emit(
+        state.copyWith(status: WalletStateStatus.success, walletDetails: walletDetails),
+      );
+    } on DioException catch (e) {
+
+      emit(state.copyWith(status: WalletStateStatus.error));
+
+    } catch (error) {
+      emit(state.copyWith(status: WalletStateStatus.error));
+    }
+  }
+
+  void _onBuyPackagesEvent(
+      BuyPackagesEvent event,
+      Emitter<WalletState> emit,
+      ) async {
+    try {
+      emit(state.copyWith(status: WalletStateStatus.loading));
+
+      final walletDetails = await walletRepository.buyInternetPackage();
       print(walletDetails);
 
       emit(
