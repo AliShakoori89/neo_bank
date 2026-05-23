4aa4e839d631913641202416a6e7f7d562460c18
a.shakouri <alishakoori89@gmail.com>
2026-05-23 14:11:28 +0330

    update custom_drop_down_shimmer.dart

--- lib/Features/Statement_Page/Presentation/Component/custom_drop_down_shimmer.dart
+++ lib/Features/Statement_Page/Presentation/Component/custom_drop_down_shimmer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:neo_bank_mehr_iran/Core/Const/app_colors.dart';
-import 'package:shimmer_animation/shimmer_animation.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CustomDropDownShimmer extends StatelessWidget {
   const CustomDropDownShimmer({super.key});
@@ -11,18 +10,14 @@ class CustomDropDownShimmer extends StatelessWidget {
       height: 40,
       width: double.infinity,
       decoration: BoxDecoration(
-        color: AppColors.circleBorderColor,
+        color: Colors.black,
         borderRadius: BorderRadius.circular(8),
       ),
-      child: Shimmer(
-        duration: Duration(seconds: 1), //Default value
-        interval: Duration(
-          microseconds: 1,
-        ), //Default value: Duration(seconds: 0)
-        //Default value
-        colorOpacity: 5, //Default value
-        enabled: true, //Default value
-        direction: ShimmerDirection.fromLTRB(), //Default Value
+      child: Shimmer.fromColors(
+        baseColor: Colors.grey[800]!,
+        highlightColor: Colors.grey[200]!,
+        period: Duration(milliseconds: 1500),
+        direction: ShimmerDirection.ltr,
         child: Container(
           decoration: BoxDecoration(
             color: Colors.white.withValues(alpha: 0.1),
