1da82064587419b1afe90cd15c38b268e38006bc
a.shakouri <alishakoori89@gmail.com>
2026-05-23 14:14:31 +0330

    edit internet_packages_repository.dart

--- lib/Features/Home_Page/Domain/Repository/internet_packages_repository.dart
+++ lib/Features/Home_Page/Domain/Repository/internet_packages_repository.dart
@@ -55,7 +55,7 @@ class InternetPackagesRepository {
       "operatorCode": operatorCode,
       "packageTimeCode": packageTimeCode,
       "simType": simType,
-      "traffic": "traffic"
+      "traffic": traffic
     };
 
     try {
