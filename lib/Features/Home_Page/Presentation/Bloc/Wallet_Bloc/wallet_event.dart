2281d223e2ba46b16e008ce3b88f7c29a585053d
a.shakouri <alishakoori89@gmail.com>
2026-05-30 09:58:11 +0330

    edit wallet_event.dart

--- lib/Features/Home_Page/Presentation/Bloc/Wallet_Bloc/wallet_event.dart
+++ lib/Features/Home_Page/Presentation/Bloc/Wallet_Bloc/wallet_event.dart
@@ -7,5 +7,28 @@ abstract class WalletEvent extends Equatable {
   List<Object?> get props => [];
 }
 
-class WalletDetailsPackagesEvent extends WalletEvent {}
-class BuyPackagesEvent extends WalletEvent {}
+class WalletDetailsPackagesEvent extends WalletEvent {
+  const WalletDetailsPackagesEvent();
+}
+
+class BuyPackagesEvent extends WalletEvent {
+  final String sourceMobileNumber;
+  final String walletAddress;
+  final int productCode;
+  final String destMobileNumber;
+
+  const BuyPackagesEvent({
+    required this.sourceMobileNumber,
+    required this.walletAddress,
+    required this.productCode,
+    required this.destMobileNumber,
+  });
+
+  @override
+  List<Object?> get props => [
+    sourceMobileNumber,
+    walletAddress,
+    productCode,
+    destMobileNumber,
+  ];
+}
