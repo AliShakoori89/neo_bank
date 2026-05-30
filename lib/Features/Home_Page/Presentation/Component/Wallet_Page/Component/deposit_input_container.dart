b6dba075ed127638169a7fb11267c56a99ee30e0
a.shakouri <alishakoori89@gmail.com>
2026-05-30 09:55:52 +0330

    edit deposit_input_container.dart

--- lib/Features/Home_Page/Presentation/Component/Wallet_Page/Component/deposit_input_container.dart
+++ lib/Features/Home_Page/Presentation/Component/Wallet_Page/Component/deposit_input_container.dart
@@ -1,30 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:neo_bank_mehr_iran/Features/Home_Page/Presentation/Component/Wallet_Page/Component/select_deposit_number_dropdown.dart';
 import '../../../../../../Core/Const/app_space.dart';
-import '../../../../../Fund_Transfer_Page/Presentation/component/Card/custom_drop_down_shimmer.dart';
-import '../../../../../Fund_Transfer_Page/Presentation/component/Card/custom_dropdown_button.dart';
-import '../../../../../Fund_Transfer_Page/Presentation/component/bank_card_selector.dart';
-import '../../../Bloc/All_cards_Bloc/all_cards_bloc.dart';
-import '../../../Bloc/All_cards_Bloc/all_cards_state.dart';
 import 'custom_formatter.dart';
 import 'input_value_text_field.dart';
 
 class DepositInputContainer extends StatefulWidget {
-  const DepositInputContainer({super.key, required this.showDepositContainer, required this.balanceController, required this.balanceFormKey});
+  const DepositInputContainer({
+    super.key,
+    required this.showDepositContainer,
+    required this.balanceController,
+    required this.balanceFormKey,
+    required this.onDepositNumberSelected,
+    this.onAmountChanged,
+  });
 
   final bool showDepositContainer;
   final CustomNumberFormatter balanceController;
   final GlobalKey<FormState> balanceFormKey;
+  final Function(String) onDepositNumberSelected;
+  final Function(int)? onAmountChanged;
 
   @override
   State<DepositInputContainer> createState() => _DepositInputContainerState();
 }
 
 class _DepositInputContainerState extends State<DepositInputContainer> {
-
-  List<String> cardDepositNumber = [];
-
   @override
   Widget build(BuildContext context) {
     return Visibility(
@@ -32,14 +32,15 @@ class _DepositInputContainerState extends State<DepositInputContainer> {
       child: Container(
         width: double.infinity,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: Colors.grey.withAlpha(25)
+          borderRadius: BorderRadius.circular(30),
+          color: Colors.grey.withAlpha(25),
         ),
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              Text('شماره حساب مورد نظر خود را انتخاب نمایید:',
+              Text(
+                'شماره حساب مورد نظر خود را انتخاب نمایید:',
                 style: TextStyle(
                   fontSize: 14,
                   fontWeight: FontWeight.w400,
@@ -47,9 +48,12 @@ class _DepositInputContainerState extends State<DepositInputContainer> {
                 ),
               ),
               AppSpace.heightSpace_16,
-              SelectDepositNumberDropdown(cardDepositNumber: cardDepositNumber,),
+              SelectDepositNumberDropdown(
+                onSelected: widget.onDepositNumberSelected,  // ✅ ارسال callback
+              ),
               AppSpace.heightSpace_24,
-              Text('مبلغ مورد نظر خود را وارد نمایید:',
+              Text(
+                'مبلغ مورد نظر خود را وارد نمایید:',
                 style: TextStyle(
                   fontSize: 14,
                   fontWeight: FontWeight.w400,
@@ -57,7 +61,11 @@ class _DepositInputContainerState extends State<DepositInputContainer> {
                 ),
               ),
               AppSpace.heightSpace_16,
-              InputValueTextField(balanceController: widget.balanceController, balanceFormKey: widget.balanceFormKey)
+              InputValueTextField(
+                balanceController: widget.balanceController,
+                balanceFormKey: widget.balanceFormKey,
+                onAmountChanged: widget.onAmountChanged,
+              ),
             ],
           ),
         ),
