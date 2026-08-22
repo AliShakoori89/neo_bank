f2a949a295c27581901990670f6be6fb062aea1a
a.shakouri <alishakoori89@gmail.com>
2026-08-22 11:31:25 +0330

    edit service item

--- lib/Features/Home_Page/Presentation/Component/Icon_Row_Widget/icon_row_widget.dart
+++ lib/Features/Home_Page/Presentation/Component/Icon_Row_Widget/icon_row_widget.dart
@@ -8,11 +8,10 @@ Widget allServicesList() {
   List<Map> servicesItem = [
     {'id' : 1 ,'itemName': 'شارژ و اینترنت', 'imagePath': 'assets/svg/bank_services_page/simcard.svg'},
     {'id' : 2 ,'itemName': 'انتقال وجه', 'imagePath': 'assets/svg/bank_services_page/switch-vertical.svg'},
-    {'id' : 3 ,'itemName': 'تسهیلات', 'imagePath': 'assets/svg/bank_services_page/receipt.svg'},
+    {'id' : 3 ,'itemName': 'تسهیلات', 'imagePath': 'assets/svg/bank_services_page/wallet.svg'},
     {'id' : 4 ,'itemName': 'قبوض', 'imagePath': 'assets/svg/bank_services_page/receipt.svg'},
-    {'id' : 5 ,'itemName': 'وام من', 'imagePath': 'assets/svg/bank_services_page/wallet.svg'},
-    {'id' : 6 ,'itemName': 'چک های من', 'imagePath': 'assets/svg/bank_services_page/ticket.svg'},
-    {'id' : 7 ,'itemName': 'هدیه آنلاین', 'imagePath': 'assets/svg/bank_services_page/gift.svg'},
+    {'id' : 5 ,'itemName': 'چک های من', 'imagePath': 'assets/svg/bank_services_page/ticket.svg'},
+    {'id' : 6 ,'itemName': 'هدیه آنلاین', 'imagePath': 'assets/svg/bank_services_page/gift.svg'},
   ];
 
   return SizedBox(
@@ -34,6 +33,8 @@ Widget allServicesList() {
                   context.push('/fund_transfer_page');
                 }else if(servicesItem[index]['id'] == 3){
                   context.push('/loan_page');
+                }else if(servicesItem[index]['id'] == 4){
+                  context.push('/invoices_page');
                 }
               },
             ),
