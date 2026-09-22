672e0e8273c12b54fe74a1b63172c4a0bc0dd41c
a.shakouri <alishakoori89@gmail.com>
2026-09-22 15:53:14 +0330

    edit code

--- lib/Features/Home_Page/Presentation/Component/Charge_Internet_Page/Component/Payment_Page/payment_page.dart
+++ lib/Features/Home_Page/Presentation/Component/Charge_Internet_Page/Component/Payment_Page/payment_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:neo_bank_mehr_iran/Features/Profile_Page/Presentation/Bloc/Profile_Bloc/profile_bloc.dart';
+import '../../../../../../../Core/DI/injection_container.dart';
 import '../../../../../../../Core/Theme/app_colors.dart';
 import '../../../../../../../Core/Spacing/app_space.dart';
 import '../../../../../../Fund_Transfer_Page/Presentation/Bloc/Account_Tab_Bloc/user_all_account_bloc.dart';
@@ -9,6 +10,7 @@ import '../../../../../../Profile_Page/Presentation/Bloc/Profile_Bloc/profile_ev
 import '../../../../../Data/Model/internet_package_model.dart';
 import '../../../../Bloc/All_cards_Bloc/all_cards_bloc.dart';
 import '../../../../Bloc/All_cards_Bloc/all_cards_event.dart';
+import '../../../../Bloc/Internet_Packages_Bloc/get_internet_packages_bloc.dart';
 import '../../../../Bloc/Wallet_Bloc/wallet_bloc.dart';
 import '../../../../Bloc/Wallet_Bloc/wallet_event.dart';
 import '../custom_header.dart';
@@ -93,17 +95,20 @@ class _PaymentPageState extends State<PaymentPage>  with SingleTickerProviderSta
                   ? buildChargePackagePaymentInfoCard(context, theme, widget.package!, widget.title, widget.amount)
                   : buildLoanInstallmentPaymentInfoCard(context, theme, widget.title, widget.amount),
               Expanded(
-                child: TabBarView(
-                  controller: _tabController,
-                  children: [
-                    BankCardPayment(amount: widget.amount, title: widget.title),
-                    WalletPayment(
-                        amount: widget.amount,
-                        title: widget.title,
-                        package: widget.package,
-                        destinationPhoneNumber: widget.phoneNumber ?? ''),
-                    BankAccountPayment(),
-                  ],
+                child: BlocProvider<InternetPackageBloc>(
+                    create: (_) => sl<InternetPackageBloc>(),
+                    child: TabBarView(
+                      controller: _tabController,
+                      children: [
+                        BankCardPayment(amount: widget.amount, title: widget.title),
+                        WalletPayment(
+                            amount: widget.amount,
+                            title: widget.title,
+                            package: widget.package,
+                            destinationPhoneNumber: widget.phoneNumber ?? ''),
+                        BankAccountPayment(),
+                      ],
+                    )
                 ),
               ),
               AppSpace.heightSpace_16,
--- lib/Features/Home_Page/Presentation/Component/Wallet_Page/Component/custom_selector_card.dart
+++ lib/Features/Home_Page/Presentation/Component/Wallet_Page/Component/custom_selector_card.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:neo_bank_mehr_iran/Core/Spacing/app_space.dart';
 import 'package:neo_bank_mehr_iran/Features/Home_Page/Data/Model/card_list_model.dart';
+import '../../../../../../Core/DI/injection_container.dart';
 import '../../../../../../Core/Theme/app_colors.dart';
+import '../../../Bloc/Balanc_visibility/balanc_visibility.dart';
 import '../../Bank_Cards_Slider/Bank_Cards/Bank_Card_Component/card_balance.dart';
 import '../../Bank_Cards_Slider/Bank_Cards/Bank_Card_Component/card_header.dart';
 import '../../Bank_Cards_Slider/Bank_Cards/Bank_Card_Component/card_number_and_date.dart';
@@ -18,79 +21,82 @@ class CustomSelectorCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Stack(
-        children: [
-          Padding(
-            padding:EdgeInsets.only(
-                right: 15
-            ),
-            child: Container(
-                width: double.infinity,
-                height: 220,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                    border: Border.all(
-                        color: isSelected ? AppColors.splashGradiantColor1 : Colors.grey,
-                        width: 2
-                    )
+      child: BlocProvider(
+          create: (_) => sl<BalanceVisibilityCubit>(),
+          child: Stack(
+            children: [
+              Padding(
+                padding:EdgeInsets.only(
+                    right: 15
                 ),
                 child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: isSelected ? AppColors.splashGradiantColor1 : Colors.grey.withAlpha(70),
-                  ),
-                    margin: EdgeInsets.only(
-                        right: 15,
-                        left: 3,
-                        bottom: 3,
-                        top: 3
+                    width: double.infinity,
+                    height: 220,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                            color: isSelected ? AppColors.splashGradiantColor1 : Colors.grey,
+                            width: 2
+                        )
                     ),
                     child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: const Color(0xFFE0E0E0), width: 2),
-                        borderRadius: BorderRadius.circular(20),
-                        color: AppColors.splashGradiantColor2.withValues(alpha: 0.1),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 0, 20, 5),
-                        child: Column(
-                          children: [
-                            buildCardHeader(),
-                            AppSpace.heightSpace_32,
-                            buildCardNumberAndDate(bankCard),
-                            CardBalanceWidget(balance: bankCard.availableBalance!),
-                          ],
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: isSelected ? AppColors.splashGradiantColor1 : Colors.grey.withAlpha(70),
                         ),
-                      ),
+                        margin: EdgeInsets.only(
+                            right: 15,
+                            left: 3,
+                            bottom: 3,
+                            top: 3
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: const Color(0xFFE0E0E0), width: 2),
+                            borderRadius: BorderRadius.circular(20),
+                            color: AppColors.splashGradiantColor2.withValues(alpha: 0.1),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 20, 5),
+                            child: Column(
+                              children: [
+                                buildCardHeader(),
+                                AppSpace.heightSpace_32,
+                                buildCardNumberAndDate(bankCard),
+                                CardBalanceWidget(balance: bankCard.availableBalance!),
+                              ],
+                            ),
+                          ),
+                        )
                     )
-                )
-            ),
-          ),
-          Positioned(
-            top: 20,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: theme.colorScheme.onPrimaryFixed,
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                      color: isSelected ? AppColors.splashGradiantColor1 : Colors.grey,
-                      width: 2
-                  )
+                ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
+              Positioned(
+                top: 20,
                 child: Container(
-                  width: 20,
-                  height: 20,
                   decoration: BoxDecoration(
-                      color: isSelected ? AppColors.splashGradiantColor1 : theme.colorScheme.onPrimaryFixed,
-                      shape: BoxShape.circle
+                      color: theme.colorScheme.onPrimaryFixed,
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                          color: isSelected ? AppColors.splashGradiantColor1 : Colors.grey,
+                          width: 2
+                      )
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                          color: isSelected ? AppColors.splashGradiantColor1 : theme.colorScheme.onPrimaryFixed,
+                          shape: BoxShape.circle
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ),
-        ],
+            ],
+          )
       ),
     );
   }
